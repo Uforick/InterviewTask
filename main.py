8e0cf723d531301b69d43fea48cc994d7cc2075c
Ilia Plotnikov <mainplotnikov@gmail.com>
2021-05-22 20:52:51 +0300

    Markup

--- main.py
+++ main.py
@@ -1,10 +1,45 @@
 from dummy_server.server import get_random_request
 
-# Dummy server generates random requests, 
-# your goal is to process them as per task requirements (see README.md)
 
-if __name__ == "__main__":
+class Request():
+    def __init__(self, type, ts, content):
+        self.type = type
+        self.ts = Time_Worck(ts)
+        self.content = content
+
+    def sort_by_type(self):
+        pass
+
+    def response(self):
+        pass
+
+
+class Time_Worck():
+    def __init__(self, ts):
+        self.ts = ts
+
+    def is_weekend(self):
+        pass
+
+
+def text_processing(is_weekend, day_of_week, content):
+    pass
+
+
+def image_processing(content, one_day_ago):
+    pass
+
+
+def video_processing(is_weekend, content):
+    pass
+
+
+def sound_processing(content):
+    pass
+
+
+if __name__ == '__main__':
     for _ in range(10):
         request = get_random_request()
         print(request)
-        # process request below
+        print(Request(*request).response())
